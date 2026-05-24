81d361d2ec634a05fd2dfef11fb2d355ca597417
astac-dev <manstac@gmail.com>
2026-05-24 00:19:42 -0600

    Fix: Ajuste de ruta base en vite.config para coincidir con el repositorio la_vidente_del_agua

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 🚀 Esto permite que funcione en local y que GitHub Pages encuentre los archivos en internet
+  base: '/la_vidente_del_agua/',
 })
